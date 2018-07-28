c6fdf47ec163ca35261b2d36a4df97383d1c2672
guardianz95 <emadpoke@gmail.com>
2018-07-27 21:08:40 -0700

    changed logic to save to file instead of user defaults

--- Plain Ol' Notes/ViewController.swift
+++ Plain Ol' Notes/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var table: UITableView!
     var mockData:[String] = []
+    var fileURL:URL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,9 @@ class ViewController: UIViewController, UITableViewDataSource {
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
         self.navigationItem.rightBarButtonItem = addButton
         self.navigationItem.leftBarButtonItem = editButtonItem
+        
+        let baseURL:URL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        fileURL = baseURL.appendingPathComponent("notes.txt")
         
         load()
     }
@@ -57,11 +61,18 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
 
     func save() {
-        UserDefaults.standard.set(mockData, forKey: "notes")
+//        UserDefaults.standard.set(mockData, forKey: "notes")
+        
+        let a = NSArray(array: mockData)
+        do {
+            try a.write(to: fileURL)
+        }catch {
+            print("error writing file")
+        }
     }
     
     func load() {
-        if let loadedData:[String] = UserDefaults.standard.value(forKey: "notes") as? [String] {
+        if let loadedData:[String] = NSArray(contentsOf: fileURL) as? [String] {
             mockData = loadedData
             table.reloadData()
         }
